e4957324cf539197efe2657da55d20ce645c0fb1
Thiago Pereira <sthiago@gmail.com>
2021-06-07 10:08:21 -0300

    Adiciona uma cor por grupo

--- main.js
+++ main.js
@@ -4,11 +4,21 @@ function parse_obj(str) {
     const normals = [];
     const faces = [];
 
+    // Lista de inteiros. Cada inteiro n significa um grupo com n faces
+    let groups = [];
+
+    let group_count = 0
     const lines = str.split("\n");
     for (let l of lines) {
         l = l.trim();
         // Pula comentários, linhas vazias, e keywords não suportadas
         if (l[0] == "" || l.startsWith("#") || l.startsWith("vt")) continue;
+
+        // Salva quantidade do grupo atual e começa novo grupo
+        if (l.startsWith("g")) {
+            groups.push(group_count);
+            group_count = 0;
+        }
 
         // Lê os vértices
         if (l.startsWith("v ")) {
@@ -37,6 +47,7 @@ function parse_obj(str) {
         // Lê as faces
         if (l.startsWith("f ")) {
             const valores = l.split(" ").filter(v => v != "");
+            group_count += 1;
 
             // Suporta apenas faces com 3 vértices (triângulos)
             if (valores.length != 4) {
@@ -78,7 +89,11 @@ function parse_obj(str) {
         }
     }
 
-    const rv = { vertices, normals, faces };
+    // Remove grupos com 0 faces
+    groups = groups.filter(i => i != 0);
+
+    const rv = { vertices, normals, faces, groups };
+    console.log(rv);
     return rv;
 }
 
@@ -97,15 +112,28 @@ function load_obj(gl, obj) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
 }
 
-function load_colors(gl, n_faces) {
+function load_colors(gl, n_faces, groups) {
     const data = [];
 
-    for (let i = 0; i < n_faces; i+=2) {
-        const colorR = Math.floor(Math.random() * 256);
-        const colorG = Math.floor(Math.random() * 256);
-        const colorB = Math.floor(Math.random() * 256);
-        data.push(colorR, colorG, colorB,colorR, colorG, colorB,colorR, colorG, colorB);
-        data.push(colorR, colorG, colorB,colorR, colorG, colorB,colorR, colorG, colorB);
+    if (groups.length == 0) {
+        // De 2 em 2 pra ficar aproximadamente 1 cor por retângulo em vez de 1 cor por
+        // triângulo
+        for (let i = 0; i < n_faces; i+=2) {
+            const colorR = Math.floor(Math.random() * 256);
+            const colorG = Math.floor(Math.random() * 256);
+            const colorB = Math.floor(Math.random() * 256);
+            data.push(colorR, colorG, colorB,colorR, colorG, colorB,colorR, colorG, colorB);
+            data.push(colorR, colorG, colorB,colorR, colorG, colorB,colorR, colorG, colorB);
+        }
+    } else {
+        for (const n_faces_no_grupo of groups) {
+            const colorR = Math.floor(Math.random() * 256);
+            const colorG = Math.floor(Math.random() * 256);
+            const colorB = Math.floor(Math.random() * 256);
+            for (let i = 0; i < n_faces_no_grupo; i++) {
+                data.push(colorR, colorG, colorB,colorR, colorG, colorB,colorR, colorG, colorB);
+            }
+        }
     }
 
     // console.log(data);
@@ -152,7 +180,7 @@ async function main() {
     // const obj = parse_obj(pot);
     // load_obj(gl, obj);
 
-    const resp = await fetch("objs/teapot.obj");
+    const resp = await fetch("objs/apples.obj");
     const str = await resp.text();
     const obj = parse_obj(str);
     load_obj(gl, obj);
@@ -165,13 +193,13 @@ async function main() {
     const colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
     // setColors(gl);
-    load_colors(gl, obj.faces.length);
+    load_colors(gl, obj.faces.length, obj.groups);
     gl.enableVertexAttribArray(a_color);
     gl.vertexAttribPointer(a_color, 3, gl.UNSIGNED_BYTE, true, 0, 0);
 
     // Valores iniciais
-    const translation = [0, 0, -500];
-    const rotation = [degToRad(0), degToRad(0), degToRad(0)];
+    const translation = [0, 0, -800];
+    const rotation = [degToRad(30), degToRad(0), degToRad(0)];
     const scale = [1, 1, 1];
 
     drawScene();
