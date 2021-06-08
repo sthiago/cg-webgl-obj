17787464cf282138214d8dfa64df7fa9da648404
Thiago Pereira <sthiago@gmail.com>
2021-06-08 17:35:43 -0300

    Substitui m4 por gl-matrix.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { mat2, mat2d, mat3, mat4, quat, quat2, vec2, vec3, vec4 } = glMatrix;
+
 /* Lê um arquivo .OBJ (suporte limitado) */
 function parse_obj(str)
 {
@@ -174,7 +176,7 @@ function find_center(vertices) {
         max: { x: xmax, y: ymax, z: zmax },
         min: { x: xmin, y: ymin, z: zmin }
     };
-    console.log(rv);
+    // console.log(rv);
 
     return rv;
 }
@@ -200,8 +202,9 @@ async function main()
     // Configuração de atributos e uniforms
     const a_position = gl.getAttribLocation(program, "a_position");
     const a_color = gl.getAttribLocation(program, "a_color");
-    const u_matrix = gl.getUniformLocation(program, "u_matrix");
-    const u_fudgefactor = gl.getUniformLocation(program, "u_fudgefactor");
+    const u_modelview = gl.getUniformLocation(program, "u_modelview");
+    const u_projectionview = gl.getUniformLocation(program, "u_projectionview");
+    const u_transform = gl.getUniformLocation(program, "u_transform");
 
     const vao = gl.createVertexArray();
     gl.bindVertexArray(vao);
@@ -225,22 +228,36 @@ async function main()
     gl.vertexAttribPointer(a_color, 3, gl.UNSIGNED_BYTE, true, 0, 0);
     load_colors(gl, obj.faces.length, obj.groups);
 
-    // Encontra centro do objeto
-    const center = find_center(obj.vertices);
+    // //////////////////////////////////////////////
 
-    // Valores do frustum
-    const near = 300;
+    // Eye -- Posição da câmera sintética
+    //        Quero que ele esteja posicionado no eixo z olhando para a origem
+    const eye = vec3.fromValues(0, 0, 10);
+
+    // ModelView -- É a matriz que representa como a câmera está posicionada no espaço.
+    //              Quero que ela esteja com o up na direção do eixo Oy mesmo
+    const yup = vec3.fromValues(0, 1, 0);
+    const origin = vec3.fromValues(0, 0, 0);
+    const modelview = mat4.create();
+    mat4.lookAt(modelview, eye, origin, yup);
+
+    // ProjectionView -- É a matriz que representa a projeção. Pode ser ortográfica ou
+    //                   perspectiva
+    const projectionview = mat4.create();
+    const fovy = degToRad(60);
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+    const near = 1;
     const far = 2000;
-    const fov = degToRad(60);
+    mat4.perspective(projectionview, fovy, aspect, near, far);
 
-    // Cálculo da distância do objeto para que fique a uma distância boa do eye
-    const d = (near * 10 * (center.max.y + center.min.y)/2) / (2 * near * Math.tan(fov/2));
+    // Transform -- É a matriz de transformação do objeto. Pode conter N transformações
+    //              em sequência
+    const zout = vec3.fromValues(0, 0, 1);
+    const transform = mat4.create();
+    mat4.rotate(transform, transform, degToRad(20), zout);
+    console.log(transform);
 
-    console.log(d);
-
-    const translation = [-center.centro.xc, -center.centro.yc, -center.centro.zc-400];
-    const rotation = [degToRad(0), degToRad(0), degToRad(0)];
-    const scale = [1, 1, 1];
+    // ////////////////////////////////////////////////
 
     drawScene();
 
@@ -256,188 +273,12 @@ async function main()
         gl.useProgram(program);
         gl.bindVertexArray(vao);
 
-        // Calcula a matriz de transformações: projeção * translação * rotações * escala
-        // No shader, essa matriz é multiplicada pela posição
-        // const matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
-        // let matrix = m4.orthographic(0, gl.canvas.clientWidth, gl.canvas.clientHeight, 0, 400, -400)
-        let matrix = m4.perspective(fov, gl.canvas.clientWidth / gl.canvas.clientHeight, near, far)
-
-        matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
-        matrix = m4.xRotate(matrix, rotation[0]);
-        matrix = m4.yRotate(matrix, rotation[1]);
-        matrix = m4.zRotate(matrix, rotation[2]);
-        matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
-
-        gl.uniformMatrix4fv(u_matrix, false, matrix);
-        gl.uniform1f(u_fudgefactor, 1.0);
+        gl.uniformMatrix4fv(u_modelview, false, modelview);
+        gl.uniformMatrix4fv(u_projectionview, false, projectionview);
+        gl.uniformMatrix4fv(u_transform, false, transform);
 
         gl.drawArrays(gl.TRIANGLES, 0, obj.faces.length * obj.vertices.length);
     }
 }
-
-var m4 = {
-    projection: function(width, height, depth) {
-        // Note: This matrix flips the Y axis so 0 is at the top.
-        return [
-            2 / width, 0, 0, 0,
-            0, -2 / height, 0, 0,
-            0, 0, 2 / depth, 0,
-            -1, 1, 0, 1,
-        ];
-    },
-
-    orthographic: function(left, right, bottom, top, near, far) {
-        return [
-            2 / (right - left), 0, 0, 0,
-            0, 2 / (top - bottom), 0, 0,
-            0, 0, 2 / (near - far), 0,
-
-            (left + right) / (left - right),
-            (bottom + top) / (bottom - top),
-            (near + far) / (near - far),
-            1,
-        ];
-    },
-
-    perspective: function(fieldOfViewInRadians, aspect, near, far) {
-        var f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
-        var rangeInv = 1.0 / (near - far);
-
-        return [
-            f / aspect, 0, 0, 0,
-            0, f, 0, 0,
-            0, 0, (near + far) * rangeInv, -1,
-            0, 0, near * far * rangeInv * 2, 0
-        ];
-    },
-
-    multiply: function(a, b) {
-        var a00 = a[0 * 4 + 0];
-        var a01 = a[0 * 4 + 1];
-        var a02 = a[0 * 4 + 2];
-        var a03 = a[0 * 4 + 3];
-        var a10 = a[1 * 4 + 0];
-        var a11 = a[1 * 4 + 1];
-        var a12 = a[1 * 4 + 2];
-        var a13 = a[1 * 4 + 3];
-        var a20 = a[2 * 4 + 0];
-        var a21 = a[2 * 4 + 1];
-        var a22 = a[2 * 4 + 2];
-        var a23 = a[2 * 4 + 3];
-        var a30 = a[3 * 4 + 0];
-        var a31 = a[3 * 4 + 1];
-        var a32 = a[3 * 4 + 2];
-        var a33 = a[3 * 4 + 3];
-        var b00 = b[0 * 4 + 0];
-        var b01 = b[0 * 4 + 1];
-        var b02 = b[0 * 4 + 2];
-        var b03 = b[0 * 4 + 3];
-        var b10 = b[1 * 4 + 0];
-        var b11 = b[1 * 4 + 1];
-        var b12 = b[1 * 4 + 2];
-        var b13 = b[1 * 4 + 3];
-        var b20 = b[2 * 4 + 0];
-        var b21 = b[2 * 4 + 1];
-        var b22 = b[2 * 4 + 2];
-        var b23 = b[2 * 4 + 3];
-        var b30 = b[3 * 4 + 0];
-        var b31 = b[3 * 4 + 1];
-        var b32 = b[3 * 4 + 2];
-        var b33 = b[3 * 4 + 3];
-
-        return [
-            b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
-            b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
-            b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
-            b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
-            b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
-            b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
-            b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
-            b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
-            b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
-            b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
-            b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
-            b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
-            b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
-            b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
-            b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
-            b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
-        ];
-    },
-
-    translation: function(tx, ty, tz) {
-        return [
-            1,  0,  0,  0,
-            0,  1,  0,  0,
-            0,  0,  1,  0,
-            tx, ty, tz, 1,
-        ];
-    },
-
-    xRotation: function(angleInRadians) {
-        var c = Math.cos(angleInRadians);
-        var s = Math.sin(angleInRadians);
-
-        return [
-            1, 0, 0, 0,
-            0, c, s, 0,
-            0, -s, c, 0,
-            0, 0, 0, 1,
-        ];
-    },
-
-    yRotation: function(angleInRadians) {
-        var c = Math.cos(angleInRadians);
-        var s = Math.sin(angleInRadians);
-
-        return [
-            c, 0, -s, 0,
-            0, 1, 0, 0,
-            s, 0, c, 0,
-            0, 0, 0, 1,
-        ];
-    },
-
-    zRotation: function(angleInRadians) {
-        var c = Math.cos(angleInRadians);
-        var s = Math.sin(angleInRadians);
-
-        return [
-            c, s, 0, 0,
-            -s, c, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1,
-        ];
-    },
-
-    scaling: function(sx, sy, sz) {
-        return [
-            sx, 0,  0,  0,
-            0, sy,  0,  0,
-            0,  0, sz,  0,
-            0,  0,  0,  1,
-        ];
-    },
-
-    translate: function(m, tx, ty, tz) {
-        return m4.multiply(m, m4.translation(tx, ty, tz));
-    },
-
-    xRotate: function(m, angleInRadians) {
-        return m4.multiply(m, m4.xRotation(angleInRadians));
-    },
-
-    yRotate: function(m, angleInRadians) {
-        return m4.multiply(m, m4.yRotation(angleInRadians));
-    },
-
-    zRotate: function(m, angleInRadians) {
-        return m4.multiply(m, m4.zRotation(angleInRadians));
-    },
-
-    scale: function(m, sx, sy, sz) {
-        return m4.multiply(m, m4.scaling(sx, sy, sz));
-    },
-};
 
 main();
